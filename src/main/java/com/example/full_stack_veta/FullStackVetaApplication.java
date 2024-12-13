0fd69c685a0fe3a87985fe33e2cdd2244f12361e
narendra <kolli7571@gmail.com>
2024-12-13 07:52:55 +0530

    first commit

---
+++ src/main/java/com/example/full_stack_veta/FullStackVetaApplication.java
@@ -0,0 +1,13 @@
+package com.example.full_stack_veta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackVetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackVetaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/full_stack_veta/FullStackVetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.full_stack_veta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackVetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
